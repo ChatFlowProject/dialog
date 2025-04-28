e1a9f6655983cbf711c11fd0fca8edd47eb77e8f
Chanui <jcu011@naver.com>
2025-04-29 01:53:39 +0900

    feat: application & Gradle 설정 추가 - DB(mysql), Kafka(prod환경 kafka cluster정보), Swagger

--- src/test/java/shop/flowchat/chat/ChatApplicationTests.java
+++ src/test/java/shop/flowchat/chat/ChatApplicationTests.java
@@ -2,8 +2,12 @@ package shop.flowchat.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @SpringBootTest
+@ActiveProfiles("dev")
 class ChatApplicationTests {
 
     @Test
