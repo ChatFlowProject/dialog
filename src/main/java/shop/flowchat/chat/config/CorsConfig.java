ae80ade1899c0382ceb7f827b29eb44b94673392
Chanui <jcu011@naver.com>
2025-05-30 22:51:15 +0900

    refactor: cors 설정 변경

--- src/main/java/shop/flowchat/chat/config/CorsConfig.java
+++ src/main/java/shop/flowchat/chat/config/CorsConfig.java
@@ -1,27 +1,26 @@
 package shop.flowchat.chat.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+    private static final List<String> ALLOWED_ORIGIN = List.of("http://localhost:3000",
+            "http://flowchat.shop:30100", "https://flowchat.shop:30200",
+            "https://chatflow-project.netlify.app");
+    private static final List<String> ALLOWED_ORIGIN_PATTERN = List.of("http://*.flowchat.shop", "https://*.flowchat.shop");
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGIN.toArray(new String[0]))
+                .allowedOriginPatterns(ALLOWED_ORIGIN_PATTERN.toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("X-Requested-With", "Origin", "Accept", "Content-Type", "Authorization", "X-XSRF-token")
+                .allowCredentials(true)
+                .maxAge(3600); // preflight 요청 결과 캐시 시간
     }
 }
